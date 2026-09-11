9ccf6670d9a8cdc661ee00190c4d386359fbe62c
jananiradhakrishnan914-ship-it <jananiradhakrishnan914@gmail.com>
2026-09-11 11:08:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,32 @@
-
-Server fixed · JS
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
- 
+
 require("dotenv").config();
- 
+
 const app = express();
- 
+
 app.use(cors());
 app.use(express.json());
- 
- 
+
+
 app.get("/", (req, res) => {
     res.send("WeatherGPT Backend is Running!");
 });
- 
- 
+
+
 app.get("/weather", async (req, res) => {
- 
+
     const city = req.query.city;
- 
+
     if (!city) {
         return res.status(400).json({
             message: "City is required"
         });
     }
- 
+
     try {
- 
+
         const response = await axios.get(
             "https://api.openweathermap.org/data/2.5/weather",
             {
@@ -39,32 +37,31 @@ app.get("/weather", async (req, res) => {
                 }
             }
         );
- 
+
         const data = response.data;
- 
+
         res.json({
             city: data.name,
             temperature: data.main.temp,
             humidity: data.main.humidity,
             condition: data.weather[0].description
         });
- 
+
     } catch (error) {
- 
+
         console.error(error.response ? error.response.data : error.message);
- 
+
         res.status(500).json({
             message: "Weather data not found"
         });
- 
+
     }
- 
+
 });
- 
- 
+
+
 const PORT = process.env.PORT || 5000;
- 
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
- 
